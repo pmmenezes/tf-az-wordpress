3c62ca69e15b4986af4d6107b46958daff6cec6f
Pablo M. Menezes <pmenezes.aju@gmail.com>
2023-09-03 23:13:00 -0300

    teste

--- rg.tf
+++ rg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-pablo-webserver-dev-eastus2"
+  name     = "rg-teste-webserver-dev-eastus2"
   location = var.regiao
   tags     = var.tags_padrao
 }
